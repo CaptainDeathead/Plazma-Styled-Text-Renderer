dab925fd7f2f45df3d28972416c9a79e7a77a819
CaptainDeathead <unstableplazma@gmail.com>
2024-05-03 16:24:36 +0800

    Added basic text rendering with line wrapping, and added basic html parsing support (bold, italic)

---
+++ example.py
@@ -0,0 +1,28 @@
+import pygame as pg
+from renderer import StyledText
+
+pg.init()
+
+screen: pg.Surface = pg.display.set_mode((800, 600))
+pg.display.set_caption("Plazma Styled Text Renderer - Example")
+
+test1: str = "this is normal <b>and this is bold</b> <i>and this is italic</i>"
+test2: str = "abcdefghijklmnopqrstuvwxyz"
+test3: str = "<b>i am not<i> and i am</i> and im not</b> and im neither"
+
+# initialize the styled text class
+styled_text: StyledText = StyledText(test3, 800, 600, (0, 0, 0), (255, 255, 255), "Comic Sans", 40, 5)
+
+clock = pg.time.Clock()
+while 1:
+    screen.fill((0, 0, 0))
+    
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            exit()
+            
+    screen.blit(styled_text.render(), (0, 0))
+    
+    clock.tick(60)
+    pg.display.flip()
---
+++ renderer.py
@@ -0,0 +1,101 @@
+import pygame as pg
+from typing import Tuple, List
+
+def get_styles(tag: str, bold: bool, italic: bool, underline: bool) -> Tuple[bool]:
+    # apply styles
+    if tag in ("b", "strong"):
+        bold = True
+    elif tag == "i":
+        italic = True
+    elif tag == "u":
+        underline = True
+    else:
+        # check if it is a closing tag
+        if tag[0] == "/":
+            # remove styles
+            if tag in ("/b", "/strong"):
+                bold = False
+            elif tag == "/i":
+                italic = False
+            elif tag == "/u":
+                underline = False
+                
+    return (bold, italic, underline)
+
+class StyledText:
+    def __init__(self, html_text: str, wrap_px: int, render_height: int, base_color: Tuple[int], background_color: Tuple[int], font_name: str, default_size: int, padding: int) -> None:
+        # setup variables
+        self.html_text: str = html_text
+        self.wrap_px: int = wrap_px
+        self.render_height: int = render_height
+        self.base_color: Tuple[int] = base_color
+        self.background_color: Tuple[int] = background_color
+        self.font_name: str = font_name
+        self.default_size: int = default_size
+        self.padding: int = padding
+        
+        # mutable variables
+        self.rendered_text: pg.Surface = pg.Surface((self.wrap_px, self.render_height))
+        
+    def render(self) -> pg.Surface:
+        # clear the previous text
+        self.rendered_text.fill(self.background_color)
+        
+        # styles
+        bold: bool = False
+        italic: bool = False
+        underline: bool = False
+        color: Tuple[int] = self.base_color
+        bg_color: Tuple[int] = self.background_color
+        font_name: str = self.font_name
+        size: int = self.default_size
+        
+        text_font: pg.Font = pg.font.SysFont(font_name, size, bold, italic)
+        
+        tag: str = ""
+        tag_text: str = ""
+        in_tag: bool = False
+        
+        curr_x: int = 0
+        curr_y: int = 0
+        
+        for char in self.html_text:
+            # entered tag
+            if char == '<':
+                in_tag = True
+                
+            # exited tag
+            elif char == '>':
+                # apply styles
+                bold, italic, underline = get_styles(tag_text, bold, italic, underline)
+                
+                # reload font with the new attributes
+                text_font = pg.font.SysFont(font_name, size, bold, italic)
+                
+                in_tag = False
+                tag_text = ""
+                
+            # in tag
+            elif in_tag:
+                if char == " ":
+                    # check for whitespace at the start
+                    if tag_text == "": continue
+                    else:
+                        tag_text += char
+                else:
+                    tag_text += char
+                    
+            # text
+            else:
+                new_char: pg.Surface = text_font.render(char, True, color, bg_color)
+                
+                # text wrapping
+                if curr_x + new_char.get_width() > self.wrap_px:
+                    curr_x = 0
+                    curr_y += new_char.get_height() + self.padding
+                    
+                self.rendered_text.blit(new_char, (curr_x, curr_y))
+                    
+                curr_x += new_char.get_width() + 1
+                
+        return self.rendered_text
