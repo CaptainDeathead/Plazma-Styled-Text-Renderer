eea6d689a9393d23f58e31c7067a1481bb9b87ba
CaptainDeathead <unstableplazma@gmail.com>
2024-05-19 15:13:39 +0800

    Split main surface into multiple depending on the size for scrolling support

--- renderer.py
+++ renderer.py
@@ -1,9 +1,9 @@
 import pygame as pg
 import logging
-import warnings
 from typing import List, Tuple, Dict
 from copy import deepcopy
 from Engine.STR.font import FontManager
+from math import floor
 
 def get_styles(tag: str, bold: bool, italic: bool, underline: bool) -> Tuple[bool]:
     line_break: bool = False
@@ -96,16 +96,22 @@ class StyledText:
         self.largest_y: float = 0
         
         # mutable variables
-        self.rendered_text: pg.Surface = pg.Surface((self.wrap_px, self.render_height))
+        self.rendered_text_screens: List[pg.Surface] = [pg.Surface((self.wrap_px, self.render_height)), pg.Surface((self.wrap_px, self.render_height))]
+        self.curr_screen: int = 0
         self.clear()
 
-    def clear(self) -> pg.Surface:
+    def clear(self) -> List[pg.Surface]:
         self.curr_x = self.base_styles["padding"][3]
         self.curr_y = self.base_styles["padding"][0]
 
+        self.rendered_text_screens = [pg.Surface((self.wrap_px, self.render_height)), pg.Surface((self.wrap_px, self.render_height))]
+        
+        for screen in self.rendered_text_screens: screen.fill((255, 255, 255))
+
+        self.curr_screen = 0
         self.renderText('\n')
-        self.rendered_text.fill(self.base_styles['background-color'])
-        return self.rendered_text
+
+        return self.rendered_text_screens
 
     def renderText(self, html_text: str, tag_styles: Dict[str, any] = None) -> Tuple[pg.Rect, pg.Rect]:
         # styles
@@ -115,7 +121,6 @@ class StyledText:
             for style in tag_styles:
                 add_style((style, tag_styles[style]), styles)
         
-        #text_font: pg.Font = pg.font.SysFont(styles['font'], styles['font-size'], styles['bold'], styles['italic'])
         text_font: pg.Font = self.font_manager.get_font(styles['font'], styles['font-size'])
         text_font.set_bold(styles['bold'])
         text_font.set_italic(styles['italic'])
@@ -187,11 +192,18 @@ class StyledText:
                     self.curr_x += 15 * (styles['font-size'] / 16)
                     self.largest_y = 0
                     
-                self.rendered_text.blit(new_char, (self.curr_x, self.curr_y))
+                self.curr_screen = floor(self.curr_y / self.render_height)
+
+                if self.curr_screen > len(self.rendered_text_screens) - 1:
+                    new_surf: pg.Surface = pg.Surface((self.wrap_px, self.render_height))
+                    new_surf.fill((255, 255, 255))
+                    self.rendered_text_screens.append(new_surf)
+
+                self.rendered_text_screens[self.curr_screen].blit(new_char, (self.curr_x, self.curr_y%self.render_height))
 
                 # underline
                 if styles['underline']:
-                    pg.draw.line(self.rendered_text, styles['color'], (self.curr_x, self.curr_y+char_height),
+                    pg.draw.line(self.rendered_text_screens[self.curr_screen], styles['color'], (self.curr_x, self.curr_y+char_height),
                                  (self.curr_x+char_width, self.curr_y+char_height))
                     
                 self.curr_x += char_width + 1
@@ -202,84 +214,3 @@ class StyledText:
         unused_rect: pg.Rect = pg.Rect(last_text_end, text_rects[-1].y, total_rect.width-last_text_end, self.largest_y)
                 
         return total_rect, unused_rect
-
-    def renderAll(self) -> pg.Surface:
-        warnings.warn("This function is inneficiant and outdated! Consider using 'renderText' instead.")
-
-        # styles
-        styles = deepcopy(self.base_styles)
-        
-        text_font: pg.Font = pg.font.SysFont(styles['font'], styles['font-size'], styles['bold'], styles['italic'])
-        
-        tag_text: str = ""
-        in_tag: bool = False
-        
-        curr_x: float = styles['padding'][3]
-        curr_y: float = styles['padding'][0]
-        largetst_y: float = 0
-        
-        for char in self.html_text:
-            # entered tag
-            if char == '<':
-                in_tag = True
-                
-            # exited tag
-            elif char == '>':
-                # apply styles
-                styles['bold'], styles['italic'], styles['underline'], line_break = get_styles(tag_text, styles['bold'], styles['italic'], styles['underline'])
-
-                # br tag
-                if line_break:
-                    curr_x, curr_y = feed_line(curr_x, curr_y, largetst_y, 16, styles['padding'])
-                    curr_x += 15 * (styles['font-size'] / 16)
-                
-                # reload font with the new attributes
-                text_font = pg.font.SysFont(styles['font'], styles['font-size'], styles['bold'], styles['italic'])
-                
-                # reset tag vars
-                in_tag = False
-                
-                tag_text = ""
-                
-            # in tag
-            elif in_tag:
-                if char == " ":
-                    # check for whitespace at the start
-                    if tag_text == "": continue
-                    else:
-                        tag_text += char
-                else:
-                    tag_text += char
-                    
-            # text
-            else:
-                # newline
-                if char == '\n':
-                    # wrap text
-                    curr_x, curr_y = feed_line(curr_x, curr_y, largetst_y, 16, styles['padding'])
-                    largetst_y = 0
-                
-                try: new_char: pg.Surface = text_font.render(char, True, styles['color'], styles['background-color'])
-                except Exception as e: logging.warning(f"Error while creating character surface! Continuing anyway...    Error: '{str(e)}'")
-
-                char_width: int = new_char.get_width()
-                char_height: int = new_char.get_height()
-
-                if char_height > largetst_y:
-                    largetst_y = char_height
-                
-                # text wrapping
-                if curr_x + char_width > self.wrap_px - styles['padding'][1]:
-                    curr_x, curr_y = feed_line(curr_x, curr_y, largetst_y, char_height, styles['padding'])
-                    curr_x += 15 * (styles['font-size'] / 16)
-                    largetst_y = 0
-                    
-                self.rendered_text.blit(new_char, (curr_x, curr_y))
-
-                # underline
-                if styles['underline']:
-                    pg.draw.line(self.rendered_text, styles['color'], (curr_x, curr_y+char_height), (curr_x+char_width, curr_y+char_height))
-                    
-                curr_x += char_width + 1
-                
-        return self.rendered_text
